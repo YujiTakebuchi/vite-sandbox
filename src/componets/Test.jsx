a6ed601d2fbb8b9781038145ffc7146dc62eb157
YujiTakebuchi <takeyou.swift@gmail.com>
2023-03-30 02:04:47 +0900

    cssモジュール検証、成功

--- src/componets/Test.jsx
+++ src/componets/Test.jsx
@@ -1,10 +1,11 @@
+import styles from "./Test.module.scss";
 import { css } from "@emotion/react";
 
 export function EmotionTest() {
   return (
-    <div className="emotion-test">
+    <div className={`${styles["emotion-test"]}`}>
       <div
-        className="emotion-test--content"
+        className={`${styles["emotion-test--content"]}`}
         css={css`
           font-weight: 700;
         `}
