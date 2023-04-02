92df1cc0a6af2e56b0da1a91f6bafd51846999fd
YujiTakebuchi <takeyou.swift@gmail.com>
2023-04-03 03:37:04 +0900

    Pictureコンポーネントのsource検証

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import variable from "./styles/var.module.scss";
 import "./App.css";
 import { EmotionTest } from "./componets/Test";
 import { Image, Picture } from "./componets/atomic/Media";
@@ -11,30 +10,16 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <EmotionTest />
       <Picture
         src={`/assets/images/too_cute.jpg`}
         srcSet={`/assets/images/too_cute@2x.jpg 2x`}
+        viewportObjectList={[
+          {
+            mediaQuery: `(max-width: ${variable.breakpoint}px)`,
+            srcSet: `/assets/images/sp/too_cute_sp@2x.jpg, /assets/images/sp/too_cute_sp@2x.jpg 2x`,
+          },
+        ]}
       />
       <Image src={`/assets/images/too_cute.jpg`} />
       <Paragraph>過去最高のベストショット</Paragraph>
