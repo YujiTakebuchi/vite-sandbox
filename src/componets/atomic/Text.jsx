bf116bfe05522f18ea0a70a2a627e74c527c2bdc
YujiTakebuchi <takeyou.swift@gmail.com>
2023-04-03 23:49:37 +0900

    H1, H2, H3コンポーネントを追加

--- src/componets/atomic/Text.jsx
+++ src/componets/atomic/Text.jsx
@@ -1,5 +1,21 @@
 import style from "./Text.module.scss";
 
+export function H1(props) {
+  return (
+    <h1 className={`${style["h1"]} ${props.cssOverrides}`}>{props.children}</h1>
+  );
+}
+export function H2(props) {
+  return (
+    <h2 className={`${style["h2"]} ${props.cssOverrides}`}>{props.children}</h2>
+  );
+}
+export function H3(props) {
+  return (
+    <h3 className={`${style["h3"]} ${props.cssOverrides}`}>{props.children}</h3>
+  );
+}
+
 export function Paragraph(props) {
   return (
     <p className={`${style["paragraph"]} ${props.cssOverrides}`}>
